d9ed8fa023c7289874765e6941c1f9557083289f
joaquin <jnbonet@gmail.com>
2021-12-14 13:13:27 -0300

    2nd commit

---
+++ main/java/filters/JspFilter.java
@@ -0,0 +1,22 @@
+package filters;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+@WebFilter("*.jsp")
+public class JspFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                          FilterChain chain)  throws IOException, ServletException {
+        RequestDispatcher dispatcher = ((HttpServletRequest) request).getRequestDispatcher("/login.jsp");
+       dispatcher.forward(request,response);
+    }
+}
